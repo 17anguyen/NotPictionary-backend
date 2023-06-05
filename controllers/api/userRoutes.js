81648ff3fe0507dd7f1d647baaa03363d801c09c
Alivia <43556891+17anguyen@users.noreply.github.com>
2023-06-05 13:45:34 -0700

    a git commit -m a git commit -m a

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,26 +3,72 @@ const { User } = require('../../models');
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt");
 
-router.get('/', async (req,res) => {
-    try {
-        const userData = await User.findAll()
-        res.status(200).json(userData)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll()
+    res.status(200).json(userData)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
+
+router.get('/:username', async (req, res) => {
+  try {
+    const findUser = await User.findOne({
+      where: {
+        username: req.params.username
+      }
+    })
+    res.status(200).json(findUser)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
 })
 
 router.post('/', async (req, res) => {
-    try {
-      const newUser = await User.create({
-        username:req.body.username,
-        password:req.body.password
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password
     });
     const token = jwt.sign(
+      {
+        username: newUser.username,
+        userId: newUser.id
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "2h"
+      }
+    );
+    res.json({
+      token,
+      user: newUser
+    });
+  } catch (err) {
+    console.log(err)
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const foundUser = await User.findOne({
+      where: {
+        username: req.body.username
+      }
+    });
+    if (!foundUser) {
+      return res.status(400).json({ msg: 'invalid login info' });
+    } else if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
+      return res.status(400).json({ msg: 'invalid login' })
+    } else {
+      const token = jwt.sign(
         {
-          username: newUser.username,
-          userId: newUser.id
+          username: foundUser.username,
+          userId: foundUser.id
         },
         process.env.JWT_SECRET,
         {
@@ -31,74 +77,42 @@ router.post('/', async (req, res) => {
       );
       res.json({
         token,
-        user: newUser
+        user: foundUser
       });
-    } catch (err) {
-      console.log(err)
-      res.status(400).json(err);
     }
-  });
-  
-router.post('/login', async (req, res) => {
-    try {
-      const foundUser = await User.findOne({ 
-        where: { 
-            username: req.body.username 
-        } 
-    });  
-      if (!foundUser) {
-        return res.status(400).json({ msg: 'invalid login info'});
-      } else if (!bcrypt.compareSync(req.body.password, foundUser.password)) {
-        return res.status(400).json({msg: 'invalid login'})
-      } else {
-        const token = jwt.sign(
-            {
-              username: foundUser.username,
-              userId: foundUser.id
-            },
-            process.env.JWT_SECRET,
-            {
-              expiresIn: "2h"
-            }
-          );
-          res.json({
-            token,
-            user: foundUser
-          });
-      } 
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-          msg: "womp womp",
-          err
-        });
-      }  
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      msg: "womp womp",
+      err
+    });
+  }
+});
 
-  router.get("/verifytoken", async (req, res) => {
-    const token = req.headers.authorization?.split(" ")[1];  
-    try {
-      const data = jwt.verify(token, process.env.JWT_SECRET);
-      const foundUser = await User.findByPk(data.userId, {
-        include: [Score]
-      });  
-      res.json(foundUser);
-    } catch (err) {
-      console.log(err);
-      res.status(403).json({ msg: "bad token", err });
-    }
-  });
-  
+router.get("/verifytoken", async (req, res) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+    const foundUser = await User.findByPk(data.userId, {
+      include: [Score]
+    });
+    res.json(foundUser);
+  } catch (err) {
+    console.log(err);
+    res.status(403).json({ msg: "bad token", err });
+  }
+});
 
-  router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.render("login");
-      });
-    } else {
-      res.status(404).end();
-    }
-  });  
-  
 
-  module.exports = router;
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.render("login");
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
+module.exports = router;
